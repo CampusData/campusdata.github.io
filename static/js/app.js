25d12aac5578f0ff7eb1944bbf5d59d0ca069bc9
Dan Schlosser <dan.r.schlosser@gmail.com>
2015-07-08 17:44:32 -0700

    cleanup forms and remove trash

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,11 @@
 $(function() {
 	$('.parallax').parallax();
-	$('.table-of-contents').pushpin({
-		top: $('.table-of-contents').offset().top - 50,
-		bottom: $('#bottom').offset().top - $(window).height()/2
-	});
+	$('.button-collapse').sideNav();
+
+	if ($('.table-of-contents').length) {
+		$('.table-of-contents').pushpin({
+			top: $('.table-of-contents').offset().top - 50,
+			bottom: $('#bottom').offset().top - $(window).height()/2
+		});
+	}
 });
